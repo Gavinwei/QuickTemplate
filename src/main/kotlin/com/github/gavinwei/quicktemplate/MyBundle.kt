847e3745fb9064a3106ff9f464107e41bdc4f726
GitHub Action <action@github.com>
2022-09-22 08:19:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/gavinwei/quicktemplate/MyBundle.kt
+++ src/main/kotlin/com/github/gavinwei/quicktemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.gavinwei.quicktemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/gavinwei/quicktemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gavinwei/quicktemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gavinwei.quicktemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.gavinwei.quicktemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
